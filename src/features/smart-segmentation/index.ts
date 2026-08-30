0c180556161aa66d7f33185046f8543cd5e73c21
Ghulam Hazrat Kooshki | Kooshk Tech <kooshktech@gmail.com>
2026-08-31 02:01:33 +0300

    VäriKamu V3: export segmentation foundation

--- src/features/smart-segmentation/index.ts
+++ src/features/smart-segmentation/index.ts
@@ -2,3 +2,6 @@ export * from './types';
 export * from './masks';
 export * from './render';
 export * from './provider';
+export * from './semanticProvider';
+export * from './history';
+export * from './webgpu';
